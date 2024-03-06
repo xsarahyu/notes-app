aef3f9959bf301027cf8dcabd77a48661ca94d35
Sarah <xsarahyu@gmail.com>
2024-03-06 18:38:53 -0500

    Initial commit

---
+++ app/src/client/auth/RequestPasswordReset.tsx
@@ -0,0 +1,10 @@
+// import { ForgotPasswordForm } from 'wasp/client/auth';
+// import { AuthWrapper } from './authWrapper';
+
+// export function RequestPasswordReset() {
+//   return (
+//     <AuthWrapper>
+//       <ForgotPasswordForm />
+//     </AuthWrapper>
+//   );
+// }
