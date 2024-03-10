4c6a42746fd1af5f2ae8ba34654791e5384487b4
socratic617 <erik.solis02135@gmail.com>
2024-03-10 19:33:38 -0400

    remove code

--- app/src/client/auth/authWrapper.test.jsx
+++ app/src/client/auth/authWrapper.test.jsx
@@ -5,9 +5,6 @@ import authWrapper from './authWrapper';
 //Test for Rendering Navigation Links:
 test('my testing test', () => {
   render(<authWrapper />);
-  const linkElements = screen.getAllByRole('link');
-  console.log(linkElements);
-  expect(linkElements.length).toBeGreaterThan(0);
   //  const element = screen.getByText('Don\'t have an account yet?') || screen.getByText('I already have an account') 
   // expect(element).toBeDefined()
   // expect(getByText('Username')).toBeInTheDocument();
