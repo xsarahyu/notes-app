fec9d2de9db0a127682ace4f7027961a941b3b38
socratic617 <erik.solis02135@gmail.com>
2024-03-11 13:05:01 -0400

    Imported mockServer and getAllTasksByUser to ensure text displayed for new note created

--- app/src/client/app/DemoAppPage.test.tsx
+++ app/src/client/app/DemoAppPage.test.tsx
@@ -1,55 +1,89 @@
 import { screen, fireEvent, waitFor } from '@testing-library/react';
-import { renderInContext } from "wasp/client/test";
+import { renderInContext, mockServer } from "wasp/client/test";
+import { getAllTasksByUser } from "wasp/client/operations";
 import DemoAppPage from './DemoAppPage';
+
+const { mockQuery} = mockServer();
 
 //Tests to focus on DemoAppPage:
 
-  // TEST 3: Clicking on checkbox for notes mockServer
-  // TEST 4: Clicking on x to delete notes mockServer
-
-  // TEST 6: See text for title "Notes App"
-  // TEST 7: 
-
-
-// describe('DemoAppPage', () => {
-//   test('renders correctly', () => {
-//     renderInContext(<DemoAppPage />);
-//     // Add your assertions here
-//   });
+// TEST 3: Clicking on checkbox for notes mockServer
+// TEST 4: Clicking on x to delete notes mockServer
 
 
   // TEST 1: Adding a Note 
   // TEST 2: See newly added note
   // TEST 5: See text default "Remember to..."  
   test('handles creating a new task', async () => {
+
+    const mockTasks = [
+      {
+        id: 1,
+        description: "test todo 1",
+        isDone: true,
+        userId: 1,
+      },
+    ];
+    
     renderInContext(<DemoAppPage />);
 
-    //to ensure you see the text default
+    //to ensure you see the text for new task 
     let input = screen.getByPlaceholderText('Remember to...');
+    fireEvent.change(input, { target: { value: mockTasks[0].description } });
+    input = screen.getByDisplayValue(mockTasks[0].description);
     expect(input).toBeInTheDocument();
-
-    //to ensure i see the text for new task 
-    fireEvent.change(input, { target: { value: 'Test task' } });
-    input = screen.getByDisplayValue('Test task');
-    expect(input).toBeInTheDocument();
-
-    //to ensure you see "Add Note" text on bttn
+    
+    //Creating new task 
     const addButton = screen.getByText('Add Note');
-    expect(addButton).toBeInTheDocument();
-
-    //Creating new task and still see the default text on bttn
     fireEvent.click(addButton);
 
+    mockQuery(getAllTasksByUser, mockTasks);
 
     // Wait for asynchronous operations, then make assertions
     await waitFor(() => {
       // Add your assertions here
-      
+      let newTask = screen.getByText(mockTasks[0].description)
+      console.log(newTask)
+      expect(newTask).toBeInTheDocument();
     });
   });
 
+
+
+  // test('Ensure default title "Notes App" is displayed', () => {
+  //   // renderInContext(<DemoAppPage />);
+  //   const titleElement = getByTextContent('Notes App');
+  //   expect(titleElement).toBeInTheDocument();
+  //   // console.log("test")
+  //   // console.log(screen.getByText((_, element) => element.textContent == 'Notes App'))
+  //   // expect(screen.getByText((_, element) => element.textContent === 'Notes App')).toBeInTheDocument();
+
+  //   // renderInContext(<DemoAppPage data-testid="component" />)
+  //   // expect(screen.getByTestId('component').textContent).toBe('Notes App')
+  // });
+
+
+  test('Verify default text displayed', async () => {
+    renderInContext(<DemoAppPage />);
+
+    //to ensure you see text "Add Note" 
+    const addButton = screen.getByText('Add Note');
+    expect(addButton).toBeInTheDocument();
+
+    //to ensure you see the text default "Remember to.."
+    let input = screen.getByPlaceholderText('Remember to...');
+    expect(input).toBeInTheDocument();
+
+    //to ensure you see the text default title "Notes App"
+    // let title = screen.getByText('Notes App');
+    // expect(title).toBeInTheDocument();
+  })
+
+  
+
+
   test('handles updating a task', async () => {
-    // Similar structure as the 'handles creating a new task' test
+    renderInContext(<DemoAppPage />);
   });
 
   test('handles deleting a task', async () => {
