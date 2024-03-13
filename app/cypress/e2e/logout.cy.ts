d36d50e42adeda0271b31294b81a6d12f455cda0
socratic617 <erik.solis02135@gmail.com>
2024-03-13 13:32:01 -0400

    Updated to use custom command, and modularize/clean code

--- app/cypress/e2e/logout.cy.ts
+++ app/cypress/e2e/logout.cy.ts
@@ -1,25 +1,9 @@
-//Sets the username as a unique user
-const timestamplogoutpage = new Date().getTime()
-const globalusernamelogoutpage = `user${timestamplogoutpage}`
-const globalpasswordlogoutpage = "newpassword1"
-
-// Creates a new user before test
-beforeEach(function() {
-  cy.visit('/signup');   
-    // Fill out the sign-up form with appropriate data
-    cy.get('input[name=username]').type(globalusernamelogoutpage);
-    cy.get('input[name=password]').type(globalpasswordlogoutpage);
-
-    // Submit the sign-up form
-    cy.get('button[type=submit]').click();
-})
-
 describe('The Logout Test', () => {
 
   it('test that you can click logout and it will log the user out', function () {
 
-    // Go back to the app main page
-    cy.visit('/demo-app')
+    //Created a Custom Command: Goes to sign up page and creates a user 
+    cy.createUser()
 
     // View the app as if it were a computer screen
     cy.viewport(1280, 800)
