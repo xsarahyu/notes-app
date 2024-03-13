a0073844dcd9bfb8ada1aa63ee59015a01e646f9
socratic617 <erik.solis02135@gmail.com>
2024-03-13 00:12:13 -0400

    Implementing my custom command "createUser"

--- app/cypress/e2e/signup_page.cy.ts
+++ app/cypress/e2e/signup_page.cy.ts
@@ -1,17 +1,8 @@
 describe('The Sign-up Page', () => {
 
   it('allows a user to sign up', () => {
-    // Visit the sign-up page
-    cy.visit('/signup');
-    
-    const username = "newuser5"
-    const password = "newpassword1"
-    // Fill out the sign-up form with appropriate data
-    cy.get('input[name=username]').type('newuser4');
-    cy.get('input[name=password]').type('newpassword1');
-
-    // Submit the sign-up form
-    cy.get('button[type=submit]').click();
+    //Created a Custom Command: Goes to sign up page and creates a user 
+    cy.createUser()
 
     // Check if the user is redirected to the expected page after signing up
     cy.url().should('include', '/demo-app');
