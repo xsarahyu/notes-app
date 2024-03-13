6f4be01e6f379032e1a791c0499893e1bf153f87
socratic617 <erik.solis02135@gmail.com>
2024-03-13 13:31:53 -0400

    Updated to use custom command, and modularize/clean code

--- app/cypress/e2e/documentation-page.cy.ts
+++ app/cypress/e2e/documentation-page.cy.ts
@@ -1,24 +1,9 @@
-//Sets the username as a unique user
-const timestampdocpage = new Date().getTime()
-const globalusernamedocpage = `user${timestampdocpage}`
-const globalpassworddocpage = "newpassword1"
-
-// Creates a new user before test
-beforeEach(function() {
-  cy.visit('/signup');   
-    // Fill out the sign-up form with appropriate data
-    cy.get('input[name=username]').type(globalusernamedocpage);
-    cy.get('input[name=password]').type(globalpassworddocpage);
-
-    // Submit the sign-up form
-    cy.get('button[type=submit]').click();
-})
-
 describe('The Documentation Page', () => {
 
   it('test that you can click the documentation page', function () {
-    // Go back to the app main page
-    cy.visit('/demo-app')
+
+    //Created a Custom Command: Goes to sign up page and creates a user 
+    cy.createUser()
 
     // View the app as if it were a computer screen
     cy.viewport(1280, 800)
@@ -32,6 +17,6 @@ describe('The Documentation Page', () => {
 
       // Test to see if the current url equals the document page
       cy.url().should('eq', 'https://docs.opensaas.sh/')
-  })
+    })
   })
 })
