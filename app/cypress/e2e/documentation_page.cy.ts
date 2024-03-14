cca3b935b2c645071cbe3e11fcc35f9638523383
socratic617 <erik.solis02135@gmail.com>
2024-03-13 22:38:47 -0400

    Cleaned up documenation

--- app/cypress/e2e/documentation_page.cy.ts
+++ app/cypress/e2e/documentation_page.cy.ts
@@ -2,12 +2,11 @@ describe('The Documentation Page', () => {
 
   it('test that you can click the documentation page', function () {
 
-    //Created a Custom Command: Goes to sign up page and creates a user 
+    // Created a Custom Command: Goes to sign up page and creates a user 
     cy.createUser()
 
     // View the app as if it were a computer screen
     cy.viewport(1280, 800)
-
 
     // Clicks on the Documentation button in the navbar
     cy.get('a').contains('Documentation').click()
